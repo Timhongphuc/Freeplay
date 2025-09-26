47bf8ec00bf6e7a3e109e804c9c74b3574762374
Tim Seufert <timseufert@MacBookPro.fritz.box>
2025-09-26 22:56:58 +0200

    Update WelcomeScreenView and FreeplayApp. (There is currently a found bug in the 'Text' section -> Variable).

---
+++ WelcomeScreenView.swift
@@ -0,0 +1,62 @@
+//
+//  WelcomeScreenView.swift
+//  Freeplay
+//
+//  Created by Tim Seufert on 26.09.25.
+//
+
+import SwiftUI
+
+struct WelcomeScreenView: View {
+    var body: some View {
+        NavigationStack{
+            Spacer(minLength: 270)
+            HStack{
+                Text("Welcome back to")
+                    .font(Font.largeTitle.bold())
+                Text("Freeplay!")
+                    .font(Font.largeTitle.bold())
+                    .foregroundStyle(
+                        LinearGradient(colors: [.blue, .purple], startPoint:
+                                .topLeading, endPoint: .bottomTrailing
+                        )
+                    )
+            } //HStack End
+            
+            HStack{
+                NavigationLink{
+                    ContentView()
+                } label: {
+                    Text("+ Start creating")
+                        .font(.system(size: 12, weight: .semibold))
+                    //                    .background(Color.blue)
+                }
+            
+            Button{
+                NSApplication.shared.terminate(nil)
+            } label: {
+                //Image(systemName: "power.circle.fill")
+                Image(systemName: "rectangle.portrait.and.arrow.right")
+                Text("Quit Freeplay")
+                    .font(.system(size: 12, weight: .semibold))
+            }
+        }
+            
+            VStack{
+                Text("Freeplay Stable Alpha 1.0, Build 228")
+                    .font(Font.caption.bold())
+                    .foregroundStyle(Color.gray)
+                
+                Spacer()
+                
+                Text("The Freeplay Project is open source at: https://github.com/Timhongphuc/Freeplay")
+                    .font(Font.caption.bold())
+            }
+            
+        } //.frame(width: 500, height: 300)
+    }
+}
+
+#Preview {
+    WelcomeScreenView()
+}
